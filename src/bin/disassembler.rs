69baf33fededf29a27949af66501891d28b9719f
Seth Barberee <seth.barberee@gmail.com>
2019-07-26 14:02:22 -0500

    reading two bytes at a time

--- src/bin/disassembler.rs
+++ src/bin/disassembler.rs
@@ -1,17 +1,30 @@
 use std::env;
 use std::fs::File;
-use std::io::BufReader;
 use std::io::prelude::*;
+
+
+fn match_instruction(counter: i64, opcode: &mut [u8]){
+    // TODO buffer holds opcode so match on the two values
+    match opcode {
+        _ => println!("Undefined Opcode on {}: {:x}{:x}", counter, opcode[0], opcode[1]),
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let file_name = env::args().into_iter().nth(1).unwrap();
-    let file = File::open(file_name).unwrap();
+    let mut file = File::open(file_name).unwrap();
+    let mut buffer = [0; 2]; // 2 bytes
     let mut pc = 0;
-    for byte in file.bytes() {
-        println!("{:x}: {:x}", pc, byte.unwrap());
-        // TODO need the next byte
-        // TODO do a match here on the bytes
-        pc += 1;
+    loop {
+        match file.read_exact(&mut buffer) {
+            Ok(T) => {
+                match_instruction(pc, &mut buffer);
+                pc += 2;
+            },
+            Err(err) => {
+                // We hit this on EOF
+                break Ok(());
+            },
+        }
     }
-    Ok(())
 }
